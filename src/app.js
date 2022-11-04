f4c534cebb09fdc5135079dd1fe7700574db76c1
gabriel yucra <gabriel.yucra_99@hotmail.com>
2022-11-04 17:47:48 -0300

    desafio 3 SERVER

--- src/app.js
+++ src/app.js
@@ -1,27 +1,21 @@
 import express from 'express'
-import Contenedor from '../desafio2/contenedor.js'
+import Contenedor from './contenedor.js'
 import fs from 'fs'
 
 const app = express(); //inciar el aplicativo
 const server = app.listen(8080,()=>console.log("Te estoy escuchando MABEL")) //Poner al aplicativo a escuchar
 
-let contenedor2 = new Contenedor("../desafio2/productos.json")
+let contenedor2 = new Contenedor("./productos.json")
 
 app.get('/productos',async (req,res)=>{
-    // let productos = await contenedor2.getAll()
-
-    // prod 
-
-    let data = await fs.promises.readFile("../desafio2/productos.json", "utf-8")
-    let prod = JSON.parse(data)
-
-
-
-    console.log(prod+'prod')
-    res.send(prod+'ola')
+    let productos = await contenedor2.getAll()
+    res.send(productos)
 })
 
-app.get('/productosRandom',(req,res)=>{
-    res.send("ragnar")
+app.get('/productosRandom', async(req,res)=>{
+    let productos = await contenedor2.getAll()
+    let rand = ~~(Math.random() * productos.length);
+    let rValue = productos[rand];
+    res.send(rValue)
 })
 
